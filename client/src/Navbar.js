e13e07950676bf8cdd8ccec4d85d91655ae492e1
Erika Szabo <erika.szabo.geo@gmail.com>
2019-10-24 07:51:16 -0400

    created navbar component

---
+++ client/src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar () {
+
+  return (
+    <nav className='navbar navbar-light bg-light'>
+        <h5 className='navbar-brand'>No Try Only Do</h5>
+    </nav>
+  )
+};
+
+export default Navbar;
