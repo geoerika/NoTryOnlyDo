e1843e6788fbff6a1d6be3e9fc6d47a00fd73b82
Erika Szabo <erika.szabo.geo@gmail.com>
2019-10-23 21:13:19 -0400

    created schema for todo

---
+++ server/models/todo.js
@@ -0,0 +1,18 @@
+// /server/todo.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure
+const DataSchema = new Schema(
+  {
+    id: Number,
+    title: { type: String, required: 'Title is required'},
+    description: String,
+    status: { type: String, required: 'Status is required'},
+    dueDate: Date
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Todo", DataSchema);
