7408982569d922979cd1b2dec0f04b9a67fac2b3
Erika Szabo <erika.szabo.geo@gmail.com>
2019-10-25 09:52:24 -0400

    implemented handleChange to deal with all form changes, also to reset state after submitting the form

--- client/src/AddTask.js
+++ client/src/AddTask.js
@@ -1,62 +1,92 @@
 import React, {Component} from 'react';
 
+//component for form which helps adding new tasks into database
 class AddTask extends Component {
 
   constructor(props) {
     super();
-    this.state = {
-      title: null,
-      description: null,
-      status: 'Pending',
-      dueDate: new Date()
-    }
+    this.state = this.getInitialState();
+    this.postTodo = this.postTodo.bind(this);
   }
 
-  getTitle = (evt) => {
-    this.setState({ title: evt.target.value })
+  //gets initial state
+  getInitialState = () => {
+    const initialState = {
+      title: '',
+      description: '',
+      status: 'Pending',
+      dueDate: '',
+      submitDisabled: true
+    };
+    return initialState;
   };
 
-  getDescription= (evt) => {
-    this.setState({ description: evt.target.value })
+  //resets state to initial values so we show empty values in the form after adding a task
+  resetState = () => {
+    this.setState(this.getInitialState());
   };
 
-  getStatus = (evt) => {
-    this.setState({ status: evt.target.value })
+  //handles change in the form and records values from the form in the state
+  handleChange = (event) => {
+    const { target: { name, value } } = event;
+    this.setState({ [name]: value });
+
+    //enable Add task button
+    if ((name === 'title') && value) {
+      this.setState({submitDisabled : false});
+    };
+
+    if ((name === 'title') && !value) {
+      this.setState({submitDisabled : true});
+    };
+
   };
 
-  getDueDate = (evt) => {
-    this.setState({ dueDate: evt.target.value })
-  };
+  //sends data to method in the App component which will send data to the database
+  postTodo = (async () => {
+    console.log('this.state: ', this.state);
+    await this.props.postDataToDB(this.state.title, this.state.description, this.state.status, this.state.dueDate);
+    this.resetState();
+  });
 
   render() {
 
     return (
 
       <div>
-        <h5>Add New Task</h5>
+        <h5 className='font-weight-bold'>Add New Task</h5>
         <hr/>
         <form>
           <div className='form-group'>
             <label>Title</label>
-            <input type='text' className='form-control' placeholder='Add title' onChange={this.getTitle}/>
+            <input type='text' className='form-control'
+                   placeholder='Add title' name='title'
+                   value={this.state.title} onChange={this.handleChange}/>
           </div>
           <div className='form-group'>
             <label>Description</label>
-            <textarea className='form-control' rows='4' placeholder='Add description' onChange={this.getDescription}/>
+            <textarea className='form-control' rows='4'
+                      placeholder='Add description' name='description'
+                      value={this.state.description} onChange={this.handleChange}/>
           </div>
           <div className="form-group">
             <label>Status</label>
-            <select className="form-control" onChange={this.getStatus}>
+            <select className="form-control" value={this.state.status}
+                    name='status' onChange={this.handleChange}>
               <option>Pending</option>
               <option>Done</option>
             </select>
           </div>
           <div className='form-group'>
             <label>Due Date</label>
-            <input type='date' className='form-control' onChange={this.getDueDate}/>
+            <input type='date' className='form-control'
+                   value={this.state.dueDate}
+                   name='dueDate' onChange={this.handleChange}/>
           </div>
         </form>
-        <button type="submit" className="btn btn-primary" onClick={() => this.props.postDataToDB(this.state.title, this.state.description, this.state.status, this.state.dueDate)}>Add task
+        <button type="submit" className="btn btn-primary"
+                disabled={this.state.submitDisabled}
+                onClick={this.postTodo}>Add task
         </button>
       </div>
     );
